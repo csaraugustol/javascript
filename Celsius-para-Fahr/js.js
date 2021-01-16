820b183ff9e3bcea79ab0f41cde51d83432eb09b
Cesar Augusto <cesar.leitejm@hotmail.com>
2021-01-15 22:50:48 -0300

    ExercJavaScript

---
+++ Celsius-para-Fahr/js.js
@@ -0,0 +1,43 @@
+function inicio(){
+    var headerElement = document.createElement('header');
+    document.body.appendChild(headerElement);
+
+    var imgElement = document.createElement('img');
+    imgElement.setAttribute('src','img/logo.png');
+    imgElement.setAttribute('id','logo');
+    document.querySelector('header').appendChild(imgElement);
+
+    var sectionElement = document.createElement('section');
+    sectionElement.setAttribute('id','sectionContainer');
+    document.body.appendChild(sectionElement);
+
+    var inputElement = document.createElement('input');
+    inputElement.setAttribute('type', 'text')
+    inputElement.setAttribute('placeholder', 'Celsius em fahrenheit')
+    inputElement.setAttribute('id', 'telaDados')
+    inputElement.setAttribute('onload','calculaResult()');
+    document.querySelector('section').appendChild(inputElement);
+
+    var calElement = document.createElement('input');
+    calElement.setAttribute('type', 'submit')
+    calElement.setAttribute('value', 'Calcular')
+    calElement.setAttribute('id','botaoCalcular')
+    calElement.setAttribute('onclick','calculaResult()')
+    document.querySelector('section').appendChild(calElement);
+
+    var sectiorDoisElement = document.createElement('div');
+    sectiorDoisElement.setAttribute('id','sesaoResultado');
+    document.body.appendChild(sectiorDoisElement);
+
+    var pElement = document.createElement('p');
+    pElement.setAttribute('id','paragrafo');
+    document.querySelector('div').appendChild(pElement);
+    
+}
+
+function calculaResult(){
+    var recebeDados = document.getElementById('telaDados').value;
+    var result = (parseFloat(recebeDados) * 1.8) + 32
+    document.getElementById('paragrafo').innerHTML = recebeDados+ "ºC = " + result + " ºF";
+    console.log(result);
+}
